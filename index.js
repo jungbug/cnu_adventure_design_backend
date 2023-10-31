115b94c1f891d2928d1a9140e4c98ab58f3bde57
JUNG SUNG YOON <xbox1111@naver.com>
2023-11-01 02:22:18 +0900

    add: team5

--- index.js
+++ index.js
@@ -39,16 +39,21 @@ app.get('/team3/login', (req, res) => {
   res.send(team3data)
 })
 
-// app.post('/team5/register', (req, res) => {
-//   console.log(req.body)
-//   team5data.push(req.body)
-//   console.log(team5data)
-//   res.send("확인이요")
-// })
+app.post('/team5/register', (req, res) => {
+  console.log(req.body)
+  team5data.forEach(element => {
+    if (element.user_id === req.body.user_id) {
+      res.send("이미 존재하는 아이디입니다.")
+    }
+  })
+  team5data.push(req.body)
+  res.send("확인이요")
+})
 
-// app.get('/team5/login', (req, res) => {
-//   res.send(team5data)
-// })
+app.get('/team5/login', (req, res) => {
+  console.log("team5 login data:", team5data)
+  res.send(team5data)
+})
 
 app.listen(3000)
 
